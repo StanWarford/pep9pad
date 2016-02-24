601e0d3fab38f34f85189e4cb90743955f04348b
Stan Warford <Stan.Warford@pepperdine.edu>
2016-02-24 14:11:19 -0800

    Initial Commit

---
+++ pep9pad/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pep9pad
+//
+//  Created by Stan Warford on 2/24/16.
+//  Copyright © 2016 Pepperdine University. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
