a5a66cb8c2105ce182380c3ebb30c9866c364d04
joshhaug <josh.haug55@gmail.com>
2016-11-16 11:35:18 -0800

    Added hooks in ASM_SourceViewController

--- pep9pad/ASM_SourceViewController.swift
+++ pep9pad/ASM_SourceViewController.swift
@@ -10,6 +10,16 @@ import UIKit
 
 class ASM_SourceViewController: UIViewController, ASM_EditorProtocol {
     
+    
+    
+    // MARK: - Properties
+    var sourceCode: [Code] = []
+    var objectCode: [Int] = []
+    var assemblerListing: [String] = []
+    var listingTrace: [String] = []
+    var hasCheckBox: [Bool] = []
+    
+    
     // MARK: - ViewController Lifecycle
     
     override func viewDidLoad() {
@@ -29,6 +39,12 @@ class ASM_SourceViewController: UIViewController, ASM_EditorProtocol {
     
     // MARK: - Methods
     
+    // Pre: self.textView.text contains a Pep/9 source program.
+    // Post: If the program assembles correctly, true is returned, and sourceCode is populated
+    // with the code objects. Otherwise false is returned and codeList is partially populated.
+    // Post: pep.symbolTable is populated with values not adjusted for .BURN.
+    // Post: pep.byteCount is the byte count for the object code not adjusted for .BURN.
+    // Post: pep.burnCount is the number of .BURN instructions encountered in the source program.
     func assemble() -> Bool {
         // PLACEHOLDER
         return true
@@ -38,6 +54,24 @@ class ASM_SourceViewController: UIViewController, ASM_EditorProtocol {
     func updateFromModel() {
         textView.setText(editorModel.source)
     }
+    
+    // Pre: self.sourceCode is populated with code from a complete correct Pep/9 source program.
+    // Post: self.objectCode is populated with the object code, one byte per entry, and returned.
+    func getObjectCode() -> [Int] {
+        // PLACEHOLDER
+        return [0]
+    }
+    
+    // Pre: self.sourceCode is populated with code from a complete correct Pep/9 source program.
+    // Post: self.assemlberListing is populated with the assembler listing.
+    // Post: self.listingTrace is populated with the object code.
+    // Post: self.hasCheckBox is populated with the checkBox list that specifies whether a trace line can have a break point.
+    // Post: assemblerListingList is returned.
+    func getAssemblerListing() -> [String] {
+        
+    }
+
+    
     
     
     
