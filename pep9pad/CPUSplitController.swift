44a6e13ebfd1018f55c6904cd8cb077a33910c83
StanWarford <Stan.Warford@pepperdine.edu>
2017-02-08 13:53:36 -0800

    Made classes for CPU tab bar

---
+++ pep9pad/CPUSplitController.swift
@@ -0,0 +1,12 @@
+//
+//  CPUSplitController.swift
+//  pep9pad
+//
+//  Created by Stan Warford on 2/8/17.
+//  Copyright © 2017 Pepperdine University. All rights reserved.
+//
+
+import UIKit
+class CPUSplitController: UIViewController {
+    
+}
