b04fb07b1340daed21b77cefb3842d69a67395a9
Frank-The-Tank <futboler101@yahoo.com>
2017-02-09 19:10:36 -0800

    updated instructions, need lots of edits

--- pep9pad/Instructions.swift
+++ pep9pad/Instructions.swift
@@ -15,7 +15,24 @@ class UnaryInstruction: Code {
     }
     
     override func appendSourceLine(assemblerListing: [String], listingTrace: [String], hasCheckBox: [Bool]) {
-        // Placeholder
+        var listingTrace = listingTrace
+        var hasCheckBox = hasCheckBox
+        var memStr: String // MARK: NEED TO UPDATE
+        var codeStr: String // MARK: NEED TO UPDATE
+        if maps.burnCount == 1 && memAddress < maps.romStartAddress {
+            codeStr = "  "
+        }
+        var symbolStr = symbolDef
+        if symbolStr.characters.count > 0 {
+            symbolStr.append(":")
+        }
+        // MARKL NEED TO DO
+        var lineStr: String = "Test" // MARK: NEED TO UPDATE
+        // MARK: NEED TO DO
+        // MARK: NEED TO DO
+        assembler.listing.append(lineStr)
+        listingTrace.append(lineStr)
+        hasCheckBox.append(true)
     }
 }
 
@@ -24,16 +41,54 @@ class NonUnaryInstruction: Code {
     private var addressingMode: EAddrMode!
     private var argument: Argument!
     override init() {}
+    
     override func appendObjectCode(objectCode: [Int]) {
-        // Placeholder
+        var objectCode = objectCode
+        if maps.burnCount == 0 || (maps.burnCount == 1 && memAddress >= maps.romStartAddress) {
+            var instructionSpecifier = maps.opCodeMap.values
+            // MARK: TO BE CONTINUED
+        }
+        // MARK: NEED TO DO
+        var operandSpecifier = argument.getArgumentValue()
+        objectCode.append(operandSpecifier / 256)
+        objectCode.append(operandSpecifier % 256)
+        
     }
     
     override func appendSourceLine(assemblerListing: [String], listingTrace: [String], hasCheckBox: [Bool]) {
-        // Placeholder
+        var listingTrace = listingTrace
+        var hasCheckBox = hasCheckBox
+        var memStr: String // MARK: NEED TO UPDATE
+        var temp: Int // MARK: NEED TO DO
+        temp += maps.addrModeRequiredMap.values ? maps.aaaAddressField(addressMode: addressingMode) : maps.aAddressField(addressMode: addressingMode)
+        var codeStr: String // MARK: NEED TO UPDATE
+        var oprndNumStr: String // MARK: NEED TO UPDATE
+        if maps.burnCount == 1 && memAddress < maps.romStartAddress {
+            codeStr = "  "
+            oprndNumStr = "   "
+        }
+        var symbolStr: String = symbolDef
+        if symbolStr.characters.count > 0 {
+            symbolStr.append(":")
+        }
+        var mnemonStr: String // MARK: NEED TO UPDATE
+        var oprndStr: String = argument.getArgumentString()
+        // MARK: NEED TO DO THIS
+        var lineStr: String = "TEST" // MARK: NEED TO UPDATE
+        // MARK: NEED TO DO THIS
+        // MARK: NEED TO DO THIS
+        assembler.listing.append(lineStr)
+        listingTrace.append(lineStr)
+        hasCheckBox.append(true)
     }
     
     override func processFormatTraceTags(sourceLine: Int, errorString: String) -> Bool {
-        // Placeholder
+        var pos: Int = assembler.rxFormatTag.index(ofAccessibilityElement: comment)
+        if pos > -1 {
+            var list: String
+            // MARK: NEED TO DO THIS
+            // TO BE CONTINUED
+        }
         return true
     }
     
