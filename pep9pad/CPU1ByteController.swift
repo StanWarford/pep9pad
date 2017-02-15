23173ed16253d844393e4d6bcb60e343962ef486
StanWarford <Stan.Warford@pepperdine.edu>
2017-02-15 12:46:34 -0800

    Added text view and cpu view to CPU split view. Added hooks for micro assembler.

---
+++ pep9pad/CPU1ByteController.swift
@@ -0,0 +1,14 @@
+//
+//  CPU1ByteController.swift
+//  pep9pad
+//
+//  Created by Stan Warford on 2/15/17.
+//  Copyright © 2017 Pepperdine University. All rights reserved.
+//
+
+import UIKit
+
+class CPU1ByteController: UIViewController {
+    
+    
+}
