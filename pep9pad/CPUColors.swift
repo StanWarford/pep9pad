ae0f8c99e43c0ae11281c41da10b5eed016c3ce0
Joseph Ramli <joseph.ramli00@gmail.com>
2017-03-13 16:42:32 -0700

    Created global, qualitative colors. Changed let to static vars in the twobytebus

--- pep9pad/CPUColors.swift
+++ pep9pad/CPUColors.swift
@@ -8,3 +8,17 @@
 import UIKit
 
 // this will contain all the colors used in both the onebyte and twobyte cpus.
+//arrow lines
+let unactivelinecolor = UIColor(red: 0.511, green: 0.583, blue: 0.670, alpha: 1.000)
+let activelinecolor = UIColor(red: 0.511, green: 0.583, blue: 0.670, alpha: 1.000)
+
+//pipes and boxes
+let unactiveshapecolor = UIColor(red: 0.511, green: 0.583, blue: 0.670, alpha: 1.000) //unactive boxes, pipes
+let ALUcolor = UIColor(red: 0.511, green: 0.583, blue: 0.670, alpha: 1.000) //ALU color never changes
+let activeABbuscolor = UIColor(red: 0.511, green: 0.583, blue: 0.670, alpha: 1.000) //A bus, B bus active
+let activeCbuscolor = UIColor(red: 0.511, green: 0.583, blue: 0.670, alpha: 1.000) //C bus active, includes Cmux and NZVC0000 arrow
+let memwritecolor = UIColor(red: 0.511, green: 0.583, blue: 0.670, alpha: 1.000) //all pipes headed to mem
+let memreadcolor = UIColor(red: 0.511, green: 0.583, blue: 0.670, alpha: 1.000) //all pipes headed from mem
+
+//registers (includes register bank, MARs, and NVZC
+let registerbackgroundcolor = UIColor(red: 0.511, green: 0.583, blue: 0.670, alpha: 1.000)
